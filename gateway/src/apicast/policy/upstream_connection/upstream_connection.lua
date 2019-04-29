7d827391e597a49db9d773c279efc838f721d375
David Ortiz <z.david.ortiz@gmail.com>
2019-04-29 12:07:15 +0200

    policy: add Upstream Connection policy

---
+++ gateway/src/apicast/policy/upstream_connection/upstream_connection.lua
@@ -0,0 +1,31 @@
+-- Upstream Connection Policy
+--- This policy exposes some parameters related with the connection to the
+--- upstream.
+
+local tonumber = tonumber
+
+local _M = require('apicast.policy').new('Upstream connection policy')
+
+local new = _M.new
+
+function _M.new(config)
+  local self = new(config)
+
+  self.connect_timeout = tonumber(config.connect_timeout)
+  self.send_timeout = tonumber(config.send_timeout)
+  self.read_timeout = tonumber(config.read_timeout)
+
+  return self
+end
+
+function _M:export()
+  return {
+    upstream_connection_opts = {
+      connect_timeout = self.connect_timeout,
+      send_timeout = self.send_timeout,
+      read_timeout = self.read_timeout
+    }
+  }
+end
+
+return _M

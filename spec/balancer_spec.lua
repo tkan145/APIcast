8cb3ba8e9667065023dd469ede6d6ed732e6ed96
David Ortiz <z.david.ortiz@gmail.com>
2019-04-29 12:04:47 +0200

    balancer: set timeouts when they are received in the context

--- spec/balancer_spec.lua
+++ spec/balancer_spec.lua
@@ -23,5 +23,37 @@ describe('apicast.balancer', function()
       assert(apicast_balancer:call({ upstream =  upstream }, balancer))
       assert.spy(balancer.set_current_peer).was.called_with(balancer, '127.0.0.2', 443)
     end)
+
+    it('sets the timeouts in the balancer when they are received in the context', function()
+      local timeouts = {
+        connect_timeout = 1,
+        send_timeout = 2,
+        read_timeout = 3,
+      }
+
+      local balancer = setmetatable({
+        set_current_peer = spy.new(function() return true end),
+        set_timeouts = spy.new(function() return true end),
+      }, { __index = apicast_balancer.default_balancer })
+
+      local upstream = Upstream.new('https://127.0.0.2')
+
+      upstream.servers = {
+        { address = '127.0.0.2', port = nil }
+      }
+
+      apicast_balancer:call(
+          { upstream =  upstream,
+            upstream_connection_opts = timeouts },
+          balancer
+      )
+
+      assert.spy(balancer.set_timeouts).was_called_with(
+          balancer,
+          timeouts.connect_timeout,
+          timeouts.send_timeout,
+          timeouts.read_timeout
+      )
+    end)
   end)
 end)

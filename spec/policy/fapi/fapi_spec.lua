b8195d2b5264df6636bedcb3d91d3cf21fbeec16
An Tran <tkan145@gmail.com>
2025-01-24 11:58:37 +1000

    [fapi] fix failing unittest test

--- spec/policy/fapi/fapi_spec.lua
+++ spec/policy/fapi/fapi_spec.lua
@@ -92,6 +92,7 @@ end)
 
 describe('fapi_1 advance profile', function()
   local context = {}
+  local ngx_req_headers = {}
   before_each(function()
     context = {
       jwt = {},
@@ -104,6 +105,8 @@ describe('fapi_1 advance profile', function()
     }
 
     ngx.header = {}
+    ngx_req_headers = {}
+    stub(ngx.req, 'get_headers', function() return ngx_req_headers end)
     stub(ngx, 'print')
     stub(ngx, 'exit')
     context.jwt = {}

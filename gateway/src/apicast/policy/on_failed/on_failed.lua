236b6dae31fe47d68e6b795df6874163ec45fbbc
An Tran <tkan145@gmail.com>
2025-03-24 14:51:20 +1000

    [on_failed] Skip body_filter and log phase

--- gateway/src/apicast/policy/on_failed/on_failed.lua
+++ gateway/src/apicast/policy/on_failed/on_failed.lua
@@ -1,6 +1,7 @@
 local _M  = require('apicast.policy').new('On failed', 'builtin')
 local new = _M.new
 
+local get_phase = ngx.get_phase
 
 function _M.new(config)
   local self = new(config)
@@ -11,6 +12,11 @@ end
 function _M:export()
   return {
     policy_error_callback = function(policy_name, error_message)
+      -- skip calling on body_filter/log as ngx.exit does not available
+      -- in these phases
+      if get_phase() == "body_filter" or get_phase() == "log" then
+        return
+      end
       ngx.log(ngx.DEBUG, "Stop request because policy: '", policy_name, "' failed, error='", error_message, "'")
       ngx.exit(self.error_status_code)
     end
